ebaa2d59c37d90f6a8386d835a1bf4824bc270c3
Jan Magnusson <jan.magnusson@sonyericsson.com>
2010-07-19 16:16:19 +0200

    Added possibility to filter commits based on branch.
    Additional parameters in addition to urls/jobs used to select manifests,
    if not starting with http:// will be treated as branches for filterning.
    Git logs for generating DMS queries, will use those branches with
    ^(caret) in front to remove from the log those commits reachable from
    the branches provided.
    
    The shortlog generated will still include all commits.
    
    New options: [--job <job URL>] [--not <ref>[,<ref>...]]
    
    Note that job is no longer a positional argument. It can
    only be provided with the --job option.

--- release-shortlog.py
+++ release-shortlog.py
@@ -5,6 +5,7 @@ import sys
 import os
 import xml.dom.minidom
 import urllib
+from optparse import OptionParser
 
 def miniparse(url):
     return xml.dom.minidom.parse(urllib.urlopen(url))
@@ -20,8 +21,26 @@ def getTextOfFirstTagByName(element, name):
 
 def main(argv):
 
-    oldBuildId = argv[1]
-    newBuildId = argv[2]
+    parser = OptionParser()
+
+    parser.add_option("-j", "--job", dest="job",
+                        default="http://android-ci.sonyericsson.net/job/offbuild_edream1.0-int/api/xml",
+                        help="An official build job URL on Hudson")
+    parser.add_option("-n", "--not", dest="not_refs",
+                        help="One or more refs (comma separated list) to exclude")
+
+    (options, args) = parser.parse_args()
+
+
+    oldBuildId = args[0]
+    newBuildId = args[1]
+
+    mainJobUrl = options.job
+
+    if options.not_refs == None:
+        notFilter = None
+    else:
+        notFilter = options.not_refs.split(',')
 
     oldManifestUrl = ""
     newManifestUrl = ""
@@ -37,10 +56,8 @@ def main(argv):
     else:
         newJobUrl = "do"
 
-    if len(argv) > 3:
-        mainJobUrl = argv[3]
-    else:
-        mainJobUrl = "http://android-ci.sonyericsson.net/job/offbuild_edream1.0-int/api/xml"
+    if not (mainJobUrl.endswith('api/xml') or mainJobUrl.endswith('api/xml/')):
+        mainJobUrl = urljoin(mainJobUrl, 'api', 'xml')
 
     if oldJobUrl == "do" or newJobUrl == "do":
         mainJobXml = miniparse(mainJobUrl)
@@ -50,11 +67,11 @@ def main(argv):
             try:
                 text = getTextOfFirstTagByName(buildJobXml, "description")
                 if oldJobUrl == "do":
-                    if text.find(oldBuildId) != -1:
+                    if text.strip() == oldBuildId:
                         oldJobUrl = buildJobUrl
                         print "Found build job url: %s" % buildJobUrl
                 if newJobUrl == "do":
-                    if text.find(newBuildId) != -1:
+                    if text.strip() == newBuildId:
                         newJobUrl = buildJobUrl
                         print "Found build job url: %s" % buildJobUrl
             except AttributeError:
@@ -72,17 +89,19 @@ def main(argv):
 
     if newManifestUrl == "":
         print >> sys.stderr, "Could not find new manifest"
+        sys.exit(1)
 
     if oldManifestUrl == "":
         print >> sys.stderr, "Could not find old manifest"
+        sys.exit(1)
 
-    generateDiff(oldManifestUrl, newManifestUrl)
+    generateDiff(oldManifestUrl, newManifestUrl, notFilter)
 
 def urljoin(first, *rest):
     return "/".join([first.rstrip('/'),
            "/".join([part.lstrip('/') for part in rest])])
 
-def generateDiff(oldManifestUri, newManifestUri):
+def generateDiff(oldManifestUri, newManifestUri, notFilter=None):
     print "Old manifest: %s" % oldManifestUri
     print "New manifest: %s" % newManifestUri
     oldManifest = miniparse(oldManifestUri)
@@ -90,6 +109,9 @@ def generateDiff(oldManifestUri, newManifestUri):
 
     newgits = []
     dmslist = []
+    commitCountFiltered = 0
+    commitCount = 0
+
     for newProject in newManifest.getElementsByTagName("project"):
         matchFound = False
         for oldProject in oldManifest.getElementsByTagName("project"):
@@ -113,10 +135,44 @@ def generateDiff(oldManifestUri, newManifestUri):
                 continue
             else:
                 print "** %s **" % newProject.getAttribute("path")
-                print "\n".join(runShortlog(newProject.getAttribute("path"), newrev, oldrev))
-                log = "\n".join(runLog(newProject.getAttribute("path"), newrev, oldrev))
+                print "\n".join(runShortlog(newProject.getAttribute("path"),
+                                            newrev,
+                                            oldrev))
+
+                log = "\n".join(runLog(newProject.getAttribute("path"),
+                                       newrev,
+                                       oldrev))
+
                 dmslist.extend(dmsqueryShow(log))
-                dmsqueryQry(log)
+
+                if notFilter != None:
+                    filteredLog = "\n".join(runLog(
+                                            newProject.getAttribute("path"),
+                                            newrev,
+                                            oldrev,
+                                            notFilter))
+
+                    dmsqueryQry(filteredLog)
+
+                    commitCountFiltered += \
+                        countCommits(newProject.getAttribute("path"),
+                                     newrev,
+                                     oldrev,
+                                     notFilter)
+
+                    commitCount += countCommits(newProject.getAttribute("path"),
+                                                newrev,
+                                                oldrev)
+                else:
+                    dmsqueryQry(log)
+
+                    commitCountFiltered += countCommits(
+                                                newProject.getAttribute("path"),
+                                                newrev,
+                                                oldrev)
+                    commitCount += countCommits(
+                                        newProject.getAttribute("path"),
+                                        newrev, oldrev)
 
         # if the project does not exist in the old manifest it must have
         # been added since then.
@@ -126,12 +182,15 @@ def generateDiff(oldManifestUri, newManifestUri):
     if len(newgits) > 0:
         print "New gits added:"
         for proj in newgits:
-            print "name=\"%s\" path=\"%s\"" % (proj.getAttribute("name"), proj.getAttribute("path"))
+            print "name=\"%s\" path=\"%s\"" % \
+                (proj.getAttribute("name"), proj.getAttribute("path"))
 
     # project name is non-volatile so it can be used as key to find the
     # the same projects in two manifests.
-    newset = set([proj.getAttribute("name") for proj in newManifest.getElementsByTagName("project")])
-    oldset = set([proj.getAttribute("name") for proj in oldManifest.getElementsByTagName("project")])
+    newset = set([proj.getAttribute("name") \
+                for proj in newManifest.getElementsByTagName("project")])
+    oldset = set([proj.getAttribute("name") \
+                for proj in oldManifest.getElementsByTagName("project")])
 
     # projects in the old manifest that can't be found in the new, must have
     # been removed.
@@ -144,27 +203,68 @@ def generateDiff(oldManifestUri, newManifestUri):
     for issue in dmslist:
         print issue
 
+    print "\nCommits introduced: %d" % commitCount
+    print "Commits used for DMS query: %d" % commitCountFiltered
+
+def countCommits(path=None, newrev=None, oldrev=None, notFilter=None):
+    rootdir = os.getcwd()
+    try:
+        os.chdir(path)
+    except OSError:
+        print >> sys.stderr, "Could not change direcotory to %s" % path
+        sys.exit(2)
+
+    cmd = "git log --pretty=oneline %s..%s" % (oldrev, newrev)
+
+    if notFilter != None:
+        for notItem in notFilter:
+            if isRef(notItem):
+                filterStr = " ^%s" % notItem
+                cmd = cmd + filterStr
+    (ret, res) = command(cmd)
+    os.chdir(rootdir)
+
+    return len(res)
+
 def runShortlog(path=None, newrev=None, oldrev=None):
     rootdir = os.getcwd()
     try:
         os.chdir(path)
     except OSError:
         print >> sys.stderr, "Could not change directory to %s" % path
+        sys.exit(2)
+
     cmd = "git shortlog --no-merges %s..%s" % (oldrev, newrev)
     (ret, res) = command(cmd)
     os.chdir(rootdir)
     return res
 
-def runLog(path=None, newrev=None, oldrev=None):
+def runLog(path=None, newrev=None, oldrev=None, notFilter=None):
     rootdir = os.getcwd()
     try:
         os.chdir(path)
     except OSError:
         print >> sys.stderr, "Could not change direcotory to %s" % path
+        sys.exit(2)
+
     cmd = "git log %s..%s" % (oldrev, newrev)
+
+    if notFilter != None:
+        for notItem in notFilter:
+            if isRef(notItem):
+                filterStr = " ^%s" % notItem
+                cmd = cmd + filterStr
+
     (ret, res) = command(cmd)
     os.chdir(rootdir)
     return res
+
+def isRef(candidate, gitpath=None):
+    cmd = "git show-ref %s" % candidate
+    (ret, res) = command(cmd)
+    if ret == 0:
+        return True
+    return False
 
 def dmsqueryQry(gitlog):
     cmd = "dmsquery -qry DMSquery.qry"
