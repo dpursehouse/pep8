37f6f318d51cd17da94a50e056d4c43479728469
Ernst Sjostrand <ernst.sjostrand@sonyericsson.com>
2011-01-17 15:46:08 +0100

    semcutil.py: Add class to parse manifests
    Central utility to parse manifests from "repo".
    Also a small documentation cleanup.
    
    Change-Id: I4ccf585fa6daa096311ff265bfd6747a481ecef8

--- semcutil.py
+++ semcutil.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
 import os
+from xml.dom.minidom import parse
+from xml.parsers.expat import ExpatError
 
 class ChildExecutionError(Exception):
     """Superclass for the various types of errors that can occur when
@@ -56,8 +58,9 @@ def run_cmd(*cmdandargs):
 
     Takes either a list with arguments or any number of arguments.
     Returns a tuple with the exit status, a string containing stdout
-    and a string containing stderr. Throws a some version of
-    ChildExecutionError if it encountered an error."""
+    and a string containing stderr. Throws some version of
+    ChildExecutionError if it encountered an error.
+    """
     if isinstance(cmdandargs[0], list):
         cmdandargs = cmdandargs[0]
     try:
@@ -75,8 +78,79 @@ def run_cmd(*cmdandargs):
         raise ChildStartupError(' '.join(cmdandargs), e)
 
 def fatal(exitcode, message):
+    """Prints an error message and does sys.exit with exitcode.
+
+    Small helper method to correctly print an error message
+    and exit the program with an exitcode.
+    """
     print >> sys.stderr, "%s: %s" % (os.path.basename(sys.argv[0]), message)
     # Shouldn't really be necessary as the stderr stream should be unbuffered.
     sys.stderr.flush()
     sys.exit(exitcode)
 
+class ManifestParseError(Exception):
+    """Raised when there is a problem opening, parsing or
+    extracting info from a repo manifest.
+    """
+    def __init__(self, problem):
+        self.problem = problem
+
+    def __str__(self):
+        return "Failed to parse manifest: %s" % (self.problem)
+
+class RepoXmlManifest():
+    """Returns an object with the info from a repo manifest.
+
+    Takes the path to a manifest as input. The object returned contains:
+
+    projects: a dict with project name as key and another dict
+    with info for each project. This dict always contains the keys "path",
+    "revision" and "name".
+    default_rev: the default revision as specified in the manifest.
+    Throws ManifestParseError on all kinds of errors.
+    """
+    def __init__(self, manifestpath):
+        self.manifestpath = manifestpath
+        self.projects = {}
+        self.default_rev = None
+
+        self._parse_manifest()
+
+    def _parse_manifest(self):
+        try:
+            domtree = parse(self.manifestpath)
+        except ExpatError, e:
+            raise ManifestParseError(str(e))
+        except IOError, e:
+            raise ManifestParseError(str(e))
+
+        default = domtree.getElementsByTagName("default")
+        if default and len(default) == 1:
+            if default[0].hasAttribute("revision"):
+                self.default_rev = default[0].attributes["revision"].nodeValue
+            else:
+                raise ManifestParseError("Missing revision attribute on the "
+                        "default tag.")
+        elif len(default) > 1:
+            raise ManifestParseError("Many default tags not allowed.")
+        else:
+            raise ManifestParseError("Missing default tag.")
+
+        for proj in domtree.getElementsByTagName("project"):
+            data = {}
+            if proj.hasAttribute("name"):
+                name = proj.attributes["name"].nodeValue
+                data["name"] = name
+            else:
+                raise ManifestParseError("Project without name attribute found.")
+
+            if proj.hasAttribute("path"):
+                data["path"] = proj.attributes["path"].nodeValue
+            else:
+                data["path"] = name
+
+            if proj.hasAttribute("revision"):
+                data["revision"] = proj.attributes["revision"].nodeValue
+            else:
+                data["revision"] = self.default_rev
+            self.projects[name] = data
