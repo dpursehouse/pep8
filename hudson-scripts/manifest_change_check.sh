bae995c953da6b749f4f55e2d3c33b5d6a8fc342
Leigh Li <leigh.li@sonyericsson.com>
2011-07-01 11:25:59 +0800

    This script is used to check manifest.git changes.
    The script is just to check if the new changes could be
    downloaded and checked out, then add code review +1 to
    Gerrit change. The submitter should make proper build and
    set verify +1.
    
    Change-Id: I1d9ca1e65e1f4987d762b55a17b6fb8531cca78b

---
+++ hudson-scripts/manifest_change_check.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+CODE_REVIEW=-1
+MSG="Buidl URL-"$BUILD_URL
+
+if [ -z "$HUDSON_REVIEWER" ]; then
+    echo "Error:HUDSON_REVIEWER is not defined. Exit."
+    exit 1
+fi
+
+rm -rf .repo/
+grep "hudsonslave" ${WORKSPACE}
+if [ "$?" -eq 0 ];then
+    rm -rf ${WORKSPACE}/*
+else
+    echo "Error:This script is only used on hudsonslave. Exit."
+    exit 1
+fi
+git clone git://review.sonyericsson.net/platform/manifest.git
+if [ "$?" -eq 0 ];then
+    cd manifest
+else
+    echo "Error:Clone manifest.git failed. Exit."
+    exit 1
+fi
+git fetch git://review.sonyericsson.net/platform/manifest.git \
+${GERRIT_REFSPEC} && git checkout FETCH_HEAD
+if [ "$?" -eq 0 ];then
+    cd ..
+else
+    echo "Error:Fetch change from Gerrit failed. Exit."
+    exit 1
+fi
+repo init -u git://review.sonyericsson.net/platform/manifest.git -b \
+${GERRIT_BRANCH} --reference=$REPO_MIRROR
+if [ "$?" -eq 0 ];then
+    cp manifest/default.xml .repo/manifests/default.xml
+else
+    echo "Error:Repo init failed. Exit."
+    exit 1
+fi
+rm -rf manifest
+repo sync --jobs=5
+if [ "$?" -eq 0 ];then
+    CODE_REVIEW=1
+fi
+ssh -p 29418 review.sonyericsson.net -l $HUDSON_REVIEWER gerrit review \
+--project=$GERRIT_PROJECT $GERRIT_PATCHSET_REVISION --code-review $CODE_REVIEW \
+\'--message="$MSG"\'
