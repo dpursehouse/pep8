6fedd708619b444ef45174bd9bbb8b5843d75f19
David Pursehouse <david.pursehouse@sonyericsson.com>
2011-10-03 16:17:14 +0900

    Jenkins verification: artifacts not archived when no Python files
    When there are no Python files in the change, archiving of the
    artifacts was failing due to missing pep8_log.txt
    
    Create a new folder called "out" in the workspace, and store the
    pep8_log.txt file in there.  Then the Jenkins job can be configured
    to archive out/** as artifacts.

--- hudson-scripts/cm_tools_verification_hudson.sh
+++ hudson-scripts/cm_tools_verification_hudson.sh
@@ -10,12 +10,16 @@ git init $PROJNAME
 git --git-dir=$PROJNAME/.git --work-tree=$PROJNAME fetch git://review.sonyericsson.net/$GERRIT_PROJECT $GERRIT_REFSPEC
 git --git-dir=$PROJNAME/.git --work-tree=$PROJNAME checkout FETCH_HEAD
 
+# Create log file
+# Workaround to prevent Jenkins error caused by trying to archive non-existent file
+echo -e "PEP-8 log:\n" > $WORKSPACE/out/pep8_log.txt
+
 # Find files that have been changed
 while IFS= read FILENAME;
 do
     # Run pep8 check on Python files
     if grep -q "\.py$" <<<$FILENAME ; then
-        python cm_tools/pep8.py -r $PROJNAME/$FILENAME | tee pep8_log.txt
+        python cm_tools/pep8.py -v -r $PROJNAME/$FILENAME | tee -a $WORKSPACE/out/pep8_log.txt
         STATUS=${PIPESTATUS[0]}
         if [ "$STATUS" -ne 0 ]; then
             EXIT_STATUS=`expr $EXIT_STATUS + 1`
--- hudson-scripts/cm_tools_verification_hudson_wrapper.sh
+++ hudson-scripts/cm_tools_verification_hudson_wrapper.sh
@@ -9,6 +9,7 @@ if [ -n "$GERRIT_CHANGE_NUMBER" ]; then
     rm -rf $WORKSPACE
     mkdir $WORKSPACE
     cd $WORKSPACE
+    mkdir out
 
     git clone git://review.sonyericsson.net/semctools/cm_tools -b master
     bash -ex ./cm_tools/hudson-scripts/cm_tools_verification_hudson.sh
