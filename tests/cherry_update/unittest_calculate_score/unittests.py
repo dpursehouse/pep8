d84a7dbe856bbb0bd51d2348ea6cb3fec2d0e007
David Pursehouse <david.pursehouse@sonyericsson.com>
2011-11-11 20:13:58 +0900

    Add cherry_update.py: script to update status of cherry picks
    This script will get all the cherry picks from the status server, and
    check them against Gerrit.  It will then update the status server with
    the merge status, code review score, and verification score.

---
+++ tests/cherry_update/unittest_calculate_score/unittests.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import unittest
+
+from cherry_update import _calculate_score
+from cherry_status import CherrypickStatusError
+
+
+class TestCalculateScore(unittest.TestCase):
+    """Test that the _calculate_score method behaves correctly.
+    """
+
+    def test_valid_scores(self):
+        """Tests that the method behaves correctly with valid data.
+        """
+        self.assertEquals(1, _calculate_score([1], -1))
+        self.assertEquals(1, _calculate_score([1, 1], -1))
+        self.assertEquals(-1, _calculate_score([1, -1], -1))
+        self.assertEquals(-1, _calculate_score([1, -1], -2))
+        self.assertEquals(-2, _calculate_score([1, -2], -2))
+        self.assertEquals(2, _calculate_score([1, -1, 2], -2))
+        self.assertEquals(-2, _calculate_score([1, 2, -2], -2))
+        self.assertEquals(-1, _calculate_score([-1, -1], -2))
+
+    def test_invalid_scores(self):
+        """Tests that the method behaves correctly with invalid data.
+        """
+        self.assertRaises(CherrypickStatusError, _calculate_score, [2], -1)
+        self.assertRaises(CherrypickStatusError, _calculate_score, [-2], -1)
+        self.assertRaises(CherrypickStatusError, _calculate_score, [0, 1], -1)
+        self.assertEquals(0, _calculate_score([], -2))
+        self.assertEquals(0, _calculate_score([], -1))
+
+
+if __name__ == '__main__':
+    unittest.main()
