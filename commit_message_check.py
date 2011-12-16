0cb4e647469e41fe0b4c09c5dd55b17048ee6ba4
David Pursehouse <david.pursehouse@sonyericsson.com>
2011-12-16 17:47:08 +0900

    commit_message_check.py: move functionality from wrapper to script
    The script is refactored to move functionality out of the wrapper
    shell script into the python script itself.  Parameters are passed
    into the script, which then downloads the change and extracts the
    commit message.
    
    Also changed to include the error/warning log in the review message,
    and only give a code review score if the change is still open and
    the patch set is current.

--- commit_message_check.py
+++ commit_message_check.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python
+
+import logging
+import optparse
+import os
 import re
 import sys
 
 import commit_message
+import gerrit
+from git import CachedGitWorkspace
+import processes
 from semcutil import enum, fatal
 
+
+# The default URL of the Gerrit server
+DEFAULT_GERRIT_SERVER = "review.sonyericsson.net"
 
 # The maximum length of a line in the commit message body
 MAX_LINE_LENGTH = 72
@@ -53,6 +64,14 @@ ERRORS = {ERROR_CODE.DMS_IN_TITLE:
           ERROR_CODE.NON_UTF8_CHARS:
             [ERROR_SEVERITY.ERROR,
                "Should not include non-UTF-8 characters."]}
+
+FAIL_MESSAGE = """Commit message does not follow the guideline:
+
+%s
+
+Please check the commit message guideline:
+https://wiki.sonyericsson.net/androiki/Commit_messages
+"""
 
 
 class CommitMessageChecker:
@@ -183,48 +202,145 @@ class CommitMessageChecker:
         return self.errors
 
 
-def check_and_display_results(results):
+def format_results(results):
     '''
-    Display the results of the commit message check.
-    Return non-zero if any errors occurred.
+    Format the results of the commit message check
+    Return the formatted results as a string, number of errors, and number
+    of warnings
     '''
-
     messages = {ERROR_SEVERITY.ERROR: "Error",
                 ERROR_SEVERITY.WARNING: "Warning"}
 
+    output = ""
     for line, code in results:
         severity, message = ERRORS[code]
-        output = "%s: " % messages[severity]
+        output += "* %s: " % messages[severity]
         if line:
             output += "Line %d: " % line
-        output += message
-        print output
+        output += message + "\n"
 
     errors = [ERRORS[c][0] for l, c in results].count(ERROR_SEVERITY.ERROR)
     warnings = [ERRORS[c][0] for l, c in results].count(ERROR_SEVERITY.WARNING)
 
-    print "\nErrors: %d" % errors
-    print "Warnings: %d" % warnings
+    output += "\nErrors: %d Warnings: %d" % (errors, warnings)
 
-    return errors
+    return output, errors, warnings
+
+
+def get_commit_message(options):
+    ''' Get the commit message from the change.
+    '''
+    patchset_ref = gerrit.get_patchset_refspec(options.change_nr,
+                                               options.patchset_nr)
+    try:
+        logging.info("Fetching patch set %s" % patchset_ref)
+        git = CachedGitWorkspace(
+            os.path.join("git://", options.gerrit_url, options.affected_git),
+            options.cache_path)
+        git.fetch(patchset_ref)
+
+        # Extract the commit message and find any DMS issues in it.
+        errcode, msg, err = processes.run_cmd("git",
+                                              "cat-file",
+                                              "-p",
+                                              "FETCH_HEAD",
+                                              path=git.git_path)
+        return commit_message.CommitMessage(msg)
+    except processes.ChildExecutionError, err:
+        fatal(2, err)
+    except EnvironmentError, err:
+        fatal(2, "Error extracting commit message: %s: %s" % \
+                 (err.strerror, err.filename))
 
 
 def main():
-    '''
-    Commit checker main function.   Expected input on stdin
-    is the output from the "git cat-file -p HEAD" command.
-    '''
+    usage = "usage: %prog [options]"
+    options = optparse.OptionParser(usage=usage)
+    options.add_option("", "--gerrit-url", dest="gerrit_url",
+                       default=DEFAULT_GERRIT_SERVER,
+                       help="The URL to the Gerrit server.")
+    options.add_option("-c", "--cache-path", dest="cache_path",
+                       default="cache",
+                       help="The path to the local directory where the " \
+                            "downloaded gits are cached to avoid cloning " \
+                            "them for each invocation of the script.")
+    options.add_option("-u", "--gerrit-user", dest="gerrit_user",
+                       default=None,
+                       help="The username that should be used when logging " \
+                            "into the Gerrit server with SSH. If omitted, " \
+                            "the SSH client will decide the username based " \
+                            "on $LOGNAME and its own configuration file " \
+                            "(if present).")
+    options.add_option("-v", "--verbose", dest="verbose", default=False,
+                       action="store_true", help="Verbose mode.")
+    options.add_option("", "--dry-run", dest="dry_run", action="store_true",
+                       help="Do everything except actually add the note " \
+                            "to the affected change.")
+    options.add_option("", "--change", dest="change_nr", type="int",
+                       help="The change number to check.")
+    options.add_option("", "--patchset", dest="patchset_nr", type="int",
+                       help="The patchset number.")
+    options.add_option("", "--project", dest="affected_git",
+                       help="The name of the project on which the " \
+                            "change is uploaded.")
+    (options, args) = options.parse_args()
+
+    if options.verbose:
+        logging.basicConfig(format='%(message)s', level=logging.INFO)
+    else:
+        logging.basicConfig(format='%(message)s', level=logging.ERROR)
+
+    if not options.change_nr:
+        fatal(1, "Change nr. missing. Use --change option.")
+    if not options.patchset_nr:
+        fatal(1, "Patchset nr. missing. Use --patchset option.")
+    if not options.affected_git:
+        fatal(1, "Project name missing. Use --project option.")
 
     try:
-        input = sys.stdin.read()
-        message = commit_message.CommitMessage(input)
+        message = get_commit_message(options)
         checker = CommitMessageChecker(message)
-        errors = checker.check()
-        exit(check_and_display_results(errors))
-    except IOError, e:
-        fatal(1, "IOError when reading input stream: %s" % (str(e)))
+        results = checker.check()
+        output, errors, warnings = format_results(results)
+        logging.info(output)
+        if (errors or warnings):
+            g = gerrit.GerritSshConnection(options.gerrit_url,
+                                           username=options.gerrit_user)
+            code_review = None
+            # If any errors have been found, set -1 code review score
+            if errors:
+                # It is possible that the change has been merged, abandoned or
+                # a new patch set uploaded during the time it has taken to run
+                # this script.
+                # Only attempt to include code review score if the change is
+                # still open and the patch set is current.
+                is_open, current_patchset = \
+                    g.change_is_open(options.change_nr)
+                if not is_open:
+                    logging.info("Change %d is closed.  Not adding code review "
+                                 "score." % options.change_nr)
+                elif options.patchset_nr != current_patchset:
+                    logging.info("Patchset %d has been replaced by patchset " \
+                                 "%d.  Not adding code review score." % \
+                                 (options.patchset_nr, current_patchset))
+                else:
+                    code_review = -1
+            if not options.dry_run:
+                g.review_patchset(change_nr=options.change_nr,
+                                  patchset=options.patchset_nr,
+                                  message=FAIL_MESSAGE % output,
+                                  codereview=code_review)
+    except gerrit.GerritSshConfigError, e:
+        fatal(1, "Error getting Gerrit ssh config: %s" % err)
+    except processes.ChildExecutionError, err:
+        fatal(2, "Error submitting review to Gerrit: %s" % err)
+    except gerrit.GerritQueryError, err:
+        fatal(3, "Error submitting review to Gerrit: %s" % err)
     except commit_message.CommitMessageError, e:
-        fatal(1, "Commit message error: %s" % (str(e)))
+        fatal(4, "Commit message error: %s" % e)
 
 if __name__ == '__main__':
-    main()
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        semcutil.fatal(5, "Interrupted by user")
--- hudson-scripts/commitcheck-hudson.sh
+++ hudson-scripts/commitcheck-hudson.sh
@@ -9,57 +9,25 @@ if [ -z "$GERRIT_USER" ]; then
     exit 1
 fi
 
-FAIL_MESSAGE="FAILED: Commit message does not follow the guideline:
-
-https://wiki.sonyericsson.net/androiki/Commit_messages
-
-See commit message check log for details:
-
-$BUILD_URL"
-
-rm -rf $WORKSPACE
-mkdir $WORKSPACE
-cd $WORKSPACE
-
-if [ -n "$GERRIT_CHANGE_NUMBER" ]; then
-    # Set up folders
-    mkdir out
-    mkdir temp
-
-    # Ignore uploaded changes on PLD gits
-    curl http://android-ci-platform.sonyericsson.net/job/pld-gits/lastSuccessfulBuild/artifact/pld-gits.txt > out/pld-gits.txt
-    if [ $(grep -c $GERRIT_PROJECT out/pld-gits.txt) -eq 1 ]; then
-        exit 0
-    fi
-
-    # Download the tools git
-    git clone git://review.sonyericsson.net/semctools/cm_tools -b master
-
-    # Download the project
-    PROJNAME=$(basename $GERRIT_PROJECT)
-    git clone git://review.sonyericsson.net/$GERRIT_PROJECT temp/$PROJNAME
-
-    # Fetch the patch set
-    cd temp/$PROJNAME
-    git fetch git://review.sonyericsson.net/$GERRIT_PROJECT $GERRIT_REFSPEC
-
-    # Checkout the head
-    git checkout FETCH_HEAD
-
-    # Get the commit message
-    git cat-file -p HEAD > $WORKSPACE/out/commit_message.txt
-
-    # Run the commit message check tool
-    cd $WORKSPACE
-    python cm_tools/commit_message_check.py < out/commit_message.txt | tee out/commit_message_check_log.txt
-    COMMIT_MESSAGE_STATUS=${PIPESTATUS[0]}
-
-    # If commit message check failed, send a review comment to Gerrit
-    if [ 0 -ne "$COMMIT_MESSAGE_STATUS" ]; then
-        ssh -p 29418 review.sonyericsson.net -l $GERRIT_USER gerrit review \
-        --project=$GERRIT_PROJECT $GERRIT_PATCHSET_REVISION \'--message="$FAIL_MESSAGE"\'
-    fi
-
-    # Exit
-    exit $COMMIT_MESSAGE_STATUS
+# Ignore uploaded changes on PLD gits
+curl http://android-ci-platform.sonyericsson.net/job/pld-gits/lastSuccessfulBuild/artifact/pld-gits.txt > out/pld-gits.txt
+if [ $(grep -c $GERRIT_PROJECT out/pld-gits.txt) -eq 1 ]; then
+    exit 0
 fi
+
+if [ -d cm_tools ] ; then
+    rm -rf cm_tools
+fi
+
+git clone git://review.sonyericsson.net/semctools/cm_tools -b master
+
+sleep 120
+
+cd cm_tools
+python commit_message_check.py \
+    --verbose \
+    --cache-path ../cache/ \
+    --change $GERRIT_CHANGE_NUMBER \
+    --project $GERRIT_PROJECT \
+    --patchset $GERRIT_PATCHSET_NUMBER \
+    --gerrit-user hudson_reviewer
