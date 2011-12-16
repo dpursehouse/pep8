52f0e73aeb88c277eb51a63300556bb860f99cfa
David Pursehouse <david.pursehouse@sonyericsson.com>
2011-12-16 13:24:19 +0900

    Add helper script to fix executable file mode
    Added a helper script to remove the executable mode from files that
    should not be executable.

---
+++ fix_executable_mode.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git ls-files -oc --exclude-standard | \
+    egrep -v '\.(pl|py|sh)$' | \
+    while read f ; do
+        if test -x "$f" && ! file -b "$f" | grep -iq executable ; then
+            echo "Shouldn't be executable: $f"
+            chmod -x "$f"
+        fi
+    done
