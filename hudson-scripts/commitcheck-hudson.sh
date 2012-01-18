bb3b063c0d48894dd94ee128e457a9dc71b380f2
David Pursehouse <david.pursehouse@sonyericsson.com>
2012-01-18 14:53:39 +0900

    commit_message_check: improvements in the Jenkins wrapper script
    Several improvements in the Jenkins wrapper script
    
    - Remove unnecessary check that $WORKSPACE is set
    - Add -x option so commands are echoed to the console
    - Use $GERRIT_USER to submit code review to Gerrit
    - If $GERRIT_USER is not specified in the parameters, use a default
    - Recreate the output directory if it already exists
    - Remove timeout; this can be done with "quiet period" in Jenkins
    - Only clone cm_tools if it does not exist, otherwise update existing

--- hudson-scripts/commitcheck-hudson.sh
+++ hudson-scripts/commitcheck-hudson.sh
@@ -1,33 +1,39 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-if [ -z "$WORKSPACE" ]; then
-    WORKSPACE="./WORKSPACE"
-fi
-
+# If GERRIT_USER is not specified in the parameters, use the default
 if [ -z "$GERRIT_USER" ]; then
-    echo "ERROR: GERRIT_USER is not set"
-    exit 1
+    GERRIT_USER="hudson_reviewer"
 fi
+
+# Remove the output directory if it already exists, and create it again
+OUTDIR="out"
+if [ -d $OUTDIR ] ; then
+    rm -rf $OUTDIR
+fi
+mkdir $OUTDIR
 
 # Ignore uploaded changes on PLD gits
-curl http://android-ci-platform.sonyericsson.net/job/pld-gits/lastSuccessfulBuild/artifact/pld-gits.txt > out/pld-gits.txt
-if [ $(grep -c $GERRIT_PROJECT out/pld-gits.txt) -eq 1 ]; then
+curl http://android-ci-platform.sonyericsson.net/job/pld-gits/lastSuccessfulBuild/artifact/pld-gits.txt > $OUTDIR/pld-gits.txt
+if [ $(grep -c $GERRIT_PROJECT $OUTDIR/pld-gits.txt) -eq 1 ]; then
     exit 0
 fi
 
+# If the cm_tools folder exists, make sure it's up to date.  If it
+# does not exist yet, create it.
 if [ -d cm_tools ] ; then
-    rm -rf cm_tools
+    cd cm_tools
+    git fetch
+    git checkout origin/master
+else
+    git clone git://review.sonyericsson.net/semctools/cm_tools -b master
+    cd cm_tools
 fi
 
-git clone git://review.sonyericsson.net/semctools/cm_tools -b master
-
-sleep 120
-
-cd cm_tools
+# Invoke the commit message checker
 python commit_message_check.py \
     --verbose \
     --cache-path ../cache/ \
     --change $GERRIT_CHANGE_NUMBER \
     --project $GERRIT_PROJECT \
     --patchset $GERRIT_PATCHSET_NUMBER \
-    --gerrit-user hudson_reviewer
+    --gerrit-user $GERRIT_USER
