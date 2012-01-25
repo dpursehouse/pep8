f453887f0ca218a9667c3ad71105c995058c5a2c
David Pursehouse <david.pursehouse@sonyericsson.com>
2012-01-25 11:00:27 +0900

    cm_check_policies: split functionality in Jenkins wrapper script
    The Jenkins wrapper script it split into two files.  The wrapper script
    that runs inside Jenkins only downloads the cm_tools and manifest gits
    and then runs a second new script that exists in cm_tools to actually
    invoke the policies check python script.
    
    With this implementation, it is not necessary to update the Jenkins job
    every time a modification needs to be made in the configuration and/or
    in how the python script is invoked.  The change can be made in the
    second script in cm_tools and then when it is merged it it will be
    automatically used in the Jenkins job.

---
+++ hudson-scripts/cm_check_policies_hudson.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -ex
+
+# To avoid nagging reviewers on every patch set, only check
+# the first patch set and then every third patch set.
+if [ `expr $GERRIT_PATCHSET_NUMBER % 3` -eq 1 ] ; then
+
+    cd cm_tools
+    python report_change_impact.py \
+        --policy etc/dms_policy.xml \
+        --verbose \
+        --cache-path ../cache/ \
+        --change $GERRIT_CHANGE_NUMBER \
+        --project $GERRIT_PROJECT \
+        --patchset $GERRIT_PATCHSET_NUMBER \
+        --branch $GERRIT_BRANCH \
+        --manifest-path ../manifest \
+        --gerrit-user hudson_reviewer \
+        --exclude-git '^ia/' \
+        --exclude-git '^edu/' \
+        --exclude-git '^semctools/(eclipse|hudson)/' \
+        --exclude-git '^kernel-2\.6$' \
+        --exclude-manifest-ref esea-feat-pldcm-tools \
+        --exclude-manifest-ref '/edream4\.0\.1-decoupled$' \
+        --exclude-manifest-ref rg-integration \
+        --exclude-manifest-ref cherrypick \
+        --exclude-manifest-ref '-int$' \
+        --exclude-manifest-ref '/ics-pitaya-viv$' \
+        --exclude-manifest-ref '/ics-mogami-(row|zeus-att|zeus-cdma|zeus-vzw)$' \
+        --exclude-manifest-ref '/ginger-ste-dev(-int2)?$' \
+        --exclude-manifest-ref '/ginger-dev$' \
+        --exclude-manifest-ref '/test-mogami-yaffs$' \
+        --exclude-manifest-ref '/adili-test-script$' \
+        --exclude-manifest-ref '-(cupcake|donut)-deckard' \
+        --exclude-manifest-ref '/sw-integration' \
+        --exclude-manifest-ref '/browser-investigation$' \
+        --exclude-manifest-ref '/donut-update$' \
+        --exclude-manifest-ref '/homesrcn$' \
+        --exclude-manifest-ref '/caf/' \
+        --exclude-manifest-ref '/ti/' \
+        --exclude-manifest-ref '-feat(ure)$' \
+        --exclude-manifest-ref '-feat(ure)?-'
+fi
--- hudson-scripts/cm_check_policies_hudson_wrapper.sh
+++ hudson-scripts/cm_check_policies_hudson_wrapper.sh
@@ -1,56 +1,22 @@
 #!/bin/bash -ex
 
+sleep 120
+
+# If the cm_tools folder exists, make sure it's up to date.  If it
+# does not exist yet, create it.
+if [ -d cm_tools ] ; then
+    ( cd cm_tools && git fetch && git checkout origin/master )
+else
+    git clone git://review.sonyericsson.net/semctools/cm_tools -b master
+fi
+
+# If the manifest folder exists, make sure it's up to date.  If it
+# does not exist yet, create it.
 if [ -d manifest ] ; then
     ( cd manifest && git fetch && git remote prune origin )
 else
     git clone git://review.sonyericsson.net/platform/manifest
 fi
 
-if [ -d cm_tools ] ; then
-    rm -rf cm_tools
-fi
-
-git clone git://review.sonyericsson.net/semctools/cm_tools -b master
-
-sleep 120
-
-# To avoid nagging reviewers on every patch set, only check
-# the first patch set and then every third patch set.
-if [ `expr $GERRIT_PATCHSET_NUMBER % 3` -eq 1 ] ; then
-
-    cd cm_tools
-    python report_change_impact.py \
-        --policy etc/dms_policy.xml \
-        --verbose \
-        --cache-path ../cache/ \
-        --change $GERRIT_CHANGE_NUMBER \
-        --project $GERRIT_PROJECT \
-        --patchset $GERRIT_PATCHSET_NUMBER \
-        --branch $GERRIT_BRANCH \
-        --manifest-path ../manifest \
-        --gerrit-user hudson_reviewer \
-        --exclude-git '^ia/' \
-        --exclude-git '^edu/' \
-        --exclude-git '^semctools/(eclipse|hudson)/' \
-        --exclude-git '^kernel-2\.6$' \
-        --exclude-manifest-ref esea-feat-pldcm-tools \
-        --exclude-manifest-ref '/edream4\.0\.1-decoupled$' \
-        --exclude-manifest-ref rg-integration \
-        --exclude-manifest-ref cherrypick \
-        --exclude-manifest-ref '-int$' \
-        --exclude-manifest-ref '/ics-pitaya-viv$' \
-        --exclude-manifest-ref '/ics-mogami-(row|zeus-att|zeus-cdma|zeus-vzw)$' \
-        --exclude-manifest-ref '/ginger-ste-dev(-int2)?$' \
-        --exclude-manifest-ref '/ginger-dev$' \
-        --exclude-manifest-ref '/test-mogami-yaffs$' \
-        --exclude-manifest-ref '/adili-test-script$' \
-        --exclude-manifest-ref '-(cupcake|donut)-deckard' \
-        --exclude-manifest-ref '/sw-integration' \
-        --exclude-manifest-ref '/browser-investigation$' \
-        --exclude-manifest-ref '/donut-update$' \
-        --exclude-manifest-ref '/homesrcn$' \
-        --exclude-manifest-ref '/caf/' \
-        --exclude-manifest-ref '/ti/' \
-        --exclude-manifest-ref '-feat(ure)$' \
-        --exclude-manifest-ref '-feat(ure)?-'
-fi
+# Invoke the cm policies checker
+cm_tools/hudson-scripts/cm_check_policies_hudson.sh 2>&1
