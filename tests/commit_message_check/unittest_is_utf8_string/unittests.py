9af0501215b854b5f782179a18d598ead33edd09
David Pursehouse <david.pursehouse@sonyericsson.com>
2012-02-16 13:47:55 +0900

    commit_message_check.py: fix pylint warnings
    Fixed several errors and warnings reported by pylint.

--- tests/commit_message_check/unittest_is_utf8_string/unittests.py
+++ tests/commit_message_check/unittest_is_utf8_string/unittests.py
@@ -5,7 +5,7 @@ import os
 import sys
 import unittest
 
-from commit_message_check import CommitMessageChecker
+from commit_message_check import is_utf8_string
 
 
 class TestIsUtf8String(unittest.TestCase):
@@ -17,16 +17,14 @@ class TestIsUtf8String(unittest.TestCase):
         is not UTF-8 encoded.
         """
         non_utf8 = "This string is not UTF-8".encode('utf-16')
-        c = CommitMessageChecker()
-        self.assertFalse(c.is_utf8_string(non_utf8))
+        self.assertFalse(is_utf8_string(non_utf8))
 
     def test_utf8_string(self):
         """Tests that the method behaves correctly for a string that
         is UTF-8 encoded.
         """
         utf8 = unicode("This string is UTF-8")
-        c = CommitMessageChecker()
-        self.assertTrue(c.is_utf8_string(utf8))
+        self.assertTrue(is_utf8_string(utf8))
 
 
 if __name__ == '__main__':
