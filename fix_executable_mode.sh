d0cb07d46b033f1cd3cb6e5a9cde7755d7795b17
David Pursehouse <david.pursehouse@sonymobile.com>
2012-04-11 18:17:21 +0900

    fix_executable_mode: exclude symbolic links from check
    Symbolic links are allowed to have the executable flag set.
    
    Change-Id: Ia35811af35a9c862e25acd2f1177c19d1a3e8d02

--- fix_executable_mode.sh
+++ fix_executable_mode.sh
@@ -3,7 +3,8 @@
 git ls-files -oc --exclude-standard | \
     egrep -v '\.(pl|py|sh)$' | \
     while read f ; do
-        if test -x "$f" && ! file -b "$f" | grep -iq executable ; then
+        if ! test -L "$f" && \
+            test -x "$f" && ! file -b "$f" | grep -iq executable ; then
             echo "Shouldn't be executable: $f"
             chmod -x "$f"
         fi
