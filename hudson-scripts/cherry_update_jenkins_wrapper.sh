4ab449746e2df007df2bab095a521cb6dfa4ba9d
David Pursehouse <david.pursehouse@sonymobile.com>
2012-04-13 16:48:59 +0900

    cherry_update/migrate: check for cm_tools in wrapper
    The Jenkins wrapper needs to check if the cm_tools git already
    exists before attempting to clone it.  If it exits, git clone
    will fail.
    
    Change-Id: I9e1631f772a0fb91a31de20b7f9a49a7bd1b02f5

--- hudson-scripts/cherry_update_jenkins_wrapper.sh
+++ hudson-scripts/cherry_update_jenkins_wrapper.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -ex
 
-# Clone the cm_tools git
-git clone git://review.sonyericsson.net/semctools/cm_tools -b master
+# If the cm_tools folder exists, make sure it's up to date.  If it
+# does not exist yet, create it.
+if [ -d cm_tools ] ; then
+    ( cd cm_tools && git fetch && git checkout origin/master )
+else
+    git clone git://review.sonyericsson.net/semctools/cm_tools -b master
+fi
 
 # Run the update script
 ./cm_tools/hudson-scripts/cherry_update_jenkins.sh
