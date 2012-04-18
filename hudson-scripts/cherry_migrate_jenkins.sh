d9a32e164c3e35d2bc234e1425fe91e07882aaa2
David Pursehouse <david.pursehouse@sonymobile.com>
2012-04-18 08:55:42 +0200

    cherry_migrate_jenkins.sh: Added edream6.1-fuji-r2-att-release branch
    Change-Id: I7ab35eb25a30233b3cfc5bee6643b56b68d7d2d6

--- hudson-scripts/cherry_migrate_jenkins.sh
+++ hudson-scripts/cherry_migrate_jenkins.sh
@@ -6,3 +6,4 @@ python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net
 python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net --new-server cmweb.sonyericsson.net --source ginger-ste --target edream6.0-riogrande-release
 python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net --new-server cmweb.sonyericsson.net --source edream6.0-riogrande-release --target edream6.0-riogrande-plus-release
 python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net --new-server cmweb.sonyericsson.net --source ics-fuji --target edream6.1-fuji-release
+python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net --new-server cmweb.sonyericsson.net --source ics-fuji --target edream6.1-fuji-r2-att-release
