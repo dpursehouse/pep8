c7c6f0ba22757a12ca40c44aa6daf6f547fdd98b
Yuuki Komiya <yuuki.x.komiya@sonymobile.com>
2012-04-18 15:02:32 +0900

    Add dms policy for edream6.1-fuji-kddi-release
    Change-Id: I115b6edec48c363c8f660c257a61fd9f2d5b1610

--- hudson-scripts/cherry_migrate_jenkins.sh
+++ hudson-scripts/cherry_migrate_jenkins.sh
@@ -7,3 +7,4 @@ python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net
 python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net --new-server cmweb.sonyericsson.net --source edream6.0-riogrande-release --target edream6.0-riogrande-plus-release
 python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net --new-server cmweb.sonyericsson.net --source ics-fuji --target edream6.1-fuji-release
 python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net --new-server cmweb.sonyericsson.net --source ics-fuji --target edream6.1-fuji-r2-att-release
+python ./cm_tools/cherry_migrate.py --old-server android-cm-web.sonyericsson.net --new-server cmweb.sonyericsson.net --source ics-fuji-kddi --target edream6.1-fuji-kddi-release
