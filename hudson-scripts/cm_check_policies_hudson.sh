1194fdba56218d9bf4c654c2c7bf70c55b3b5d43
David Pursehouse <david.pursehouse@sonymobile.com>
2012-05-03 12:22:40 +0900

    cm_check_policies: Add support for checking AMSS manifest
    Updated the check policies script to also check for impact on AMSS
    manifest.
    
    Change-Id: I6668491b31354c94b7217002b5b92dbd2cd88f5b

--- hudson-scripts/cm_check_policies_hudson.sh
+++ hudson-scripts/cm_check_policies_hudson.sh
@@ -3,40 +3,41 @@
 # To avoid nagging reviewers on every patch set, only check
 # the first patch set and then every third patch set.
 if [ `expr $GERRIT_PATCHSET_NUMBER % 3` -eq 1 ] ; then
-
-    cd cm_tools
-    python report_change_impact.py \
-        --policy etc/dms_policy.xml \
-        --verbose \
-        --change $GERRIT_CHANGE_NUMBER \
-        --project $GERRIT_PROJECT \
-        --patchset $GERRIT_PATCHSET_NUMBER \
-        --branch $GERRIT_BRANCH \
-        --revision $GERRIT_PATCHSET_REVISION \
-        --manifest-path ../manifest \
-        --gerrit-user hudson_reviewer \
-        --exclude-git '^ia/' \
-        --exclude-git '^edu/' \
-        --exclude-git '^semctools/(eclipse|hudson)/' \
-        --exclude-git '^kernel-2\.6$' \
-        --exclude-manifest-ref esea-feat-pldcm-tools \
-        --exclude-manifest-ref '/edream4\.0\.1-decoupled$' \
-        --exclude-manifest-ref rg-integration \
-        --exclude-manifest-ref cherrypick \
-        --exclude-manifest-ref '-int$' \
-        --exclude-manifest-ref '/ics-blue-qct-mw$' \
-        --exclude-manifest-ref '/ics-pitaya-viv$' \
-        --exclude-manifest-ref '/ginger-ste-dev(-int2)?$' \
-        --exclude-manifest-ref '/ginger-dev$' \
-        --exclude-manifest-ref '/test-mogami-yaffs$' \
-        --exclude-manifest-ref '/adili-test-script$' \
-        --exclude-manifest-ref '-(cupcake|donut)-deckard' \
-        --exclude-manifest-ref '/sw-integration' \
-        --exclude-manifest-ref '/browser-investigation$' \
-        --exclude-manifest-ref '/donut-update$' \
-        --exclude-manifest-ref '/homesrcn$' \
-        --exclude-manifest-ref '/caf/' \
-        --exclude-manifest-ref '/ti/' \
-        --exclude-manifest-ref '-feat(ure)$' \
-        --exclude-manifest-ref '-feat(ure)?-'
+    for m in "$@"
+    do
+        python cm_tools/report_change_impact.py \
+            --policy cm_tools/etc/dms_policy.xml \
+            --verbose \
+            --change $GERRIT_CHANGE_NUMBER \
+            --project $GERRIT_PROJECT \
+            --patchset $GERRIT_PATCHSET_NUMBER \
+            --branch $GERRIT_BRANCH \
+            --revision $GERRIT_PATCHSET_REVISION \
+            --manifest-path $m \
+            --gerrit-user hudson_reviewer \
+            --exclude-git '^ia/' \
+            --exclude-git '^edu/' \
+            --exclude-git '^semctools/(eclipse|hudson)/' \
+            --exclude-git '^kernel-2\.6$' \
+            --exclude-manifest-ref esea-feat-pldcm-tools \
+            --exclude-manifest-ref '/edream4\.0\.1-decoupled$' \
+            --exclude-manifest-ref rg-integration \
+            --exclude-manifest-ref cherrypick \
+            --exclude-manifest-ref '-int$' \
+            --exclude-manifest-ref '/ics-blue-qct-mw$' \
+            --exclude-manifest-ref '/ics-pitaya-viv$' \
+            --exclude-manifest-ref '/ginger-ste-dev(-int2)?$' \
+            --exclude-manifest-ref '/ginger-dev$' \
+            --exclude-manifest-ref '/test-mogami-yaffs$' \
+            --exclude-manifest-ref '/adili-test-script$' \
+            --exclude-manifest-ref '-(cupcake|donut)-deckard' \
+            --exclude-manifest-ref '/sw-integration' \
+            --exclude-manifest-ref '/browser-investigation$' \
+            --exclude-manifest-ref '/donut-update$' \
+            --exclude-manifest-ref '/homesrcn$' \
+            --exclude-manifest-ref '/caf/' \
+            --exclude-manifest-ref '/ti/' \
+            --exclude-manifest-ref '-feat(ure)$' \
+            --exclude-manifest-ref '-feat(ure)?-'
+    done
 fi
--- hudson-scripts/cm_check_policies_hudson_wrapper.sh
+++ hudson-scripts/cm_check_policies_hudson_wrapper.sh
@@ -2,21 +2,25 @@
 
 sleep 120
 
-# If the cm_tools folder exists, make sure it's up to date.  If it
-# does not exist yet, create it.
+# If the cm_tools git exists, make sure it's up to date.  If it
+# does not exist yet, clone it.
 if [ -d cm_tools ] ; then
     ( cd cm_tools && git fetch && git checkout origin/master )
 else
     git clone git://review.sonyericsson.net/semctools/cm_tools -b master
 fi
 
-# If the manifest folder exists, make sure it's up to date.  If it
-# does not exist yet, create it.
-if [ -d manifest ] ; then
-    ( cd manifest && git fetch && git remote prune origin )
-else
-    git clone git://review.sonyericsson.net/platform/manifest
-fi
+# If the manifest gits exist, make sure they are up to date.  If they
+# do not exist yet, clone them.
+CM_CHECK_MANIFESTS=("manifest" "amssmanifest")
+for m in "${CM_CHECK_MANIFESTS[@]}"
+do
+    if [ -d $m ] ; then
+        ( cd $m && git fetch && git remote prune origin )
+    else
+        git clone git://review.sonyericsson.net/platform/$m
+    fi
+done
 
 # Invoke the cm policies checker
-cm_tools/hudson-scripts/cm_check_policies_hudson.sh 2>&1
+cm_tools/hudson-scripts/cm_check_policies_hudson.sh ${CM_CHECK_MANIFESTS[@]} 2>&1
