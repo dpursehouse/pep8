8e63e980ee62b58a6de8797df9e2b2372dca31f0
David Pursehouse <david.pursehouse@sonymobile.com>
2012-06-04 10:51:34 +0900

    cm_tools_verification: Set maximum Python line length to 80 chars
    The default setting in pep8.py is 79 characters, but at SOMC the
    limit is 80.
    
    The latest version of pep8.py has a command line option to set the
    maximum characters.  Update the verification script to set this
    parameter, rather than hard-coding the change in the pep8 script
    itself.
    
    Change-Id: Icc37cd6062a3c6c716d5606ea8e389c6138b8f92

--- hudson-scripts/cm_tools_verification_hudson.sh
+++ hudson-scripts/cm_tools_verification_hudson.sh
@@ -30,6 +30,7 @@ do
         # Run PEP-8 check on Python files
         if grep -q "\.py$" <<<$FILENAME ; then
             python cm_tools/pep8.py -v -r --show-source --show-pep8 \
+                --max-line-length 80 \
                 $PROJNAME/$FILENAME | tee -a $WORKSPACE/out/pep8_log.txt
             STATUS=${PIPESTATUS[0]}
             if [ "$STATUS" -ne 0 ]; then
