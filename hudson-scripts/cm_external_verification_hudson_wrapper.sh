26179e821c0e1976a63bd1a314a703448ccb49a9
Vijayaraghavan Surendran <vijayaraghavan.x.surendran@sonymobile.com>
2012-06-20 17:03:52 +0900

    Handle decoupled-deliveries specially in external packages verification
    Since decoupled-deliveries is not a single git and the `delivery.xml`
    is found under respective application sub-folder clone the gits in
    the same folder structure that is required to run the test cases
    successfully.
    
    Also added command to list the files that will be included in the
    test cases.
    
    Change-Id: I6c997f29d6ea1e91d81d8087c9fed65becc880b0

--- hudson-scripts/cm_external_verification_hudson_wrapper.sh
+++ hudson-scripts/cm_external_verification_hudson_wrapper.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 if [ -z "$WORKSPACE" ]; then
     echo "ERROR: WORKSPACE is not set"
@@ -15,17 +15,28 @@ sleep 30
 #The python scripts used in the test cases are present in semcsystem git.
 #Hence it is required to have the semcsystem git for the tests to run successfully
 
-PROJ_DIR=`echo $GERRIT_PROJECT | sed 's#^.*/##'`
+PROJ_DIR=
+FILES=
+if [ `echo $GERRIT_PROJECT | grep 'decoupled-deliveries/.*'` ] ; then
+    PROJ_DIR=`echo $GERRIT_PROJECT | sed 's#^.*/\(decoupled-deliveries/.*\)#\1#'`
+    FILES='*.xml'
+else
+    PROJ_DIR=`echo $GERRIT_PROJECT | sed 's#^.*/##'`
+    FILES='package-files/*.xml'
+fi
+
 rm -rf $PROJ_DIR
 mkdir -p $PROJ_DIR
 cd $PROJ_DIR
 git init
 git pull git://review.sonyericsson.net/$GERRIT_PROJECT $GERRIT_REFSPEC
-cd ..
+cd $WORKSPACE
+
+# List all the files that will be included in the test cases
+ls -x $PROJ_DIR/$FILES
 
 git clone git://review.sonyericsson.net/semctools/semcsystem -b ginger-common
 cd semcsystem
-
 if [ -d tests ]; then
     make -C tests external-packages
 fi
